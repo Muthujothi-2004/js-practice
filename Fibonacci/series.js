8aa74e421e35c5bc7770bb37569ada0ebafbfdef
Muthujothi-2004 <muthujothi24680@gmail.com>
2025-01-28 13:00:12 +0530

    balnceSums

--- Fibonacci/series.js
+++ Fibonacci/series.js
@@ -43,19 +43,51 @@
 
 // febbo(10);
 
-function feb(n){
-    let a=0;
-    let b=1;
-    let c=0;
-    let arr=[];
-    for(i=1;i<=n;i++){
-      if(a<10){
-        arr.push(a);
-      }
-        c=a+b;
-          a=b;
-          b=c;
-  }
-    console.log(arr[arr.length-1])  
+// function feb(n){
+//     let a=0;
+//     let b=1;
+//     let c=0;
+//     let arr=[];
+//     for(i=1;i<=n;i++){
+//       if(a<10){
+//         arr.push(a);
+//       }
+//         c=a+b;
+//           a=b;
+//           b=c;
+//   }
+//     console.log(arr.join(' '))  
+//     }
+// feb(10)
+
+
+// function fibonacci(n) {
+//   // Enter your code here
+// let a=0;
+// let b=1;
+// let c=0;
+//   for(i=3;i<=n.length;i++){
+//       console.log(a);
+//         if(a<=b){
+//             c=a+b;
+//             a=a
+//             b=c
+//         }
+//     }
+//     console.log(c);
+//   }
+//   fibonacci(8)
+
+let list1=[1,3,4,5]
+ let list2=[1,3,5,4]
+
+let str="";
+
+for(let i=0; i<list1.length; i++){
+  for(let j=0; j<list2.length; j++){
+    if(list1[i]===list2[j]){
+      str=str+list1[i]+", ";
     }
-feb(10)
+  }
+}
+console.log(str.slice(0,str.length-2));
